443b2581097b74a9b6bd57151a4ad549df204588
Ryotankobu <ucp4fya7@yahoo.co.jp>
2025-12-10 16:30:21 +1000

    POR-1006 Project Section Base Design Completed

--- src/components/ProjectSection.vue
+++ src/components/ProjectSection.vue
@@ -1,5 +1,6 @@
 <script setup>
 import SectionHeader from './common/SectionHeader.vue';
+import sampleImage from "@/assets/images/bellsprout.png";
 </script>
 
 <template>
@@ -14,77 +15,168 @@ import SectionHeader from './common/SectionHeader.vue';
             <div class="row pt-5">
                 <div class="col-4 p-0 pe-2">
                     <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <img class="card-img-top w-50 mx-auto d-block" :src="sampleImage" alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                            <div class="language-section d-flex flex-row mb-2">
+                                <div class="language-box border rounded px-3 me-2">
+                                    React
+                                </div>
+                                <div class="language-box border rounded px-3">
+                                    React
+                                </div>
+                            </div>
+                            <h5 class="card-title">Project Title Will Be Here</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <hr class="border-bottom border-2 border-dark">
+                            <div class="project-button-section d-flex flex-row justify-content-between">
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <div class="project-icon-section">
+                                    <a href="#" class="btn btn-primary me-3">B</a>
+                                    <a href="#" class="btn btn-primary">B</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-4 p-0 px-2">
-                    <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4 p-0 ps-2">
-                    <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <
-            </div>
 
-            <div class="row pt-4">
                 <div class="col-4 p-0 pe-2">
                     <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <img class="card-img-top w-50 mx-auto d-block" :src="sampleImage" alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                            <div class="language-section d-flex flex-row mb-2">
+                                <div class="language-box border rounded px-3 me-2">
+                                    React
+                                </div>
+                                <div class="language-box border rounded px-3">
+                                    React
+                                </div>
+                            </div>
+                            <h5 class="card-title">Project Title Will Be Here</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <hr class="border-bottom border-2 border-dark">
+                            <div class="project-button-section d-flex flex-row justify-content-between">
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <div class="project-icon-section">
+                                    <a href="#" class="btn btn-primary me-3">B</a>
+                                    <a href="#" class="btn btn-primary">B</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-4 p-0 px-2">
-                    <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4 p-0 ps-2">
-                    <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <
-            </div>
-            
-        </div>
-        <!-- <div class="experiencce-section-main container-fluid bg-primary flex-grow-1 p-0 d-flex flex-column">
-            <div class="card work-experience-card mt-4">aa</div>
-            <div class="card study-experience-card flex-grow-1 mt-4">aaa</div>
 
-        </div> -->
+                <div class="col-4 p-0 pe-2">
+                    <div class="card">
+                        <img class="card-img-top w-50 mx-auto d-block" :src="sampleImage" alt="Card image cap">
+                        <div class="card-body">
+                            <div class="language-section d-flex flex-row mb-2">
+                                <div class="language-box border rounded px-3 me-2">
+                                    React
+                                </div>
+                                <div class="language-box border rounded px-3">
+                                    React
+                                </div>
+                            </div>
+                            <h5 class="card-title">Project Title Will Be Here</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <hr class="border-bottom border-2 border-dark">
+                            <div class="project-button-section d-flex flex-row justify-content-between">
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <div class="project-icon-section">
+                                    <a href="#" class="btn btn-primary me-3">B</a>
+                                    <a href="#" class="btn btn-primary">B</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+             <div class="row pt-5">
+                <div class="col-4 p-0 pe-2">
+                    <div class="card">
+                        <img class="card-img-top w-50 mx-auto d-block" :src="sampleImage" alt="Card image cap">
+                        <div class="card-body">
+                            <div class="language-section d-flex flex-row mb-2">
+                                <div class="language-box border rounded px-3 me-2">
+                                    React
+                                </div>
+                                <div class="language-box border rounded px-3">
+                                    React
+                                </div>
+                            </div>
+                            <h5 class="card-title">Project Title Will Be Here</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <hr class="border-bottom border-2 border-dark">
+                            <div class="project-button-section d-flex flex-row justify-content-between">
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <div class="project-icon-section">
+                                    <a href="#" class="btn btn-primary me-3">B</a>
+                                    <a href="#" class="btn btn-primary">B</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-4 p-0 pe-2">
+                    <div class="card">
+                        <img class="card-img-top w-50 mx-auto d-block" :src="sampleImage" alt="Card image cap">
+                        <div class="card-body">
+                            <div class="language-section d-flex flex-row mb-2">
+                                <div class="language-box border rounded px-3 me-2">
+                                    React
+                                </div>
+                                <div class="language-box border rounded px-3">
+                                    React
+                                </div>
+                            </div>
+                            <h5 class="card-title">Project Title Will Be Here</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <hr class="border-bottom border-2 border-dark">
+                            <div class="project-button-section d-flex flex-row justify-content-between">
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <div class="project-icon-section">
+                                    <a href="#" class="btn btn-primary me-3">B</a>
+                                    <a href="#" class="btn btn-primary">B</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-4 p-0 pe-2">
+                    <div class="card">
+                        <img class="card-img-top w-50 mx-auto d-block" :src="sampleImage" alt="Card image cap">
+                        <div class="card-body">
+                            <div class="language-section d-flex flex-row mb-2">
+                                <div class="language-box border rounded px-3 me-2">
+                                    React
+                                </div>
+                                <div class="language-box border rounded px-3">
+                                    React
+                                </div>
+                            </div>
+                            <h5 class="card-title">Project Title Will Be Here</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <hr class="border-bottom border-2 border-dark">
+                            <div class="project-button-section d-flex flex-row justify-content-between">
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <div class="project-icon-section">
+                                    <a href="#" class="btn btn-primary me-3">B</a>
+                                    <a href="#" class="btn btn-primary">B</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
 
     </div>

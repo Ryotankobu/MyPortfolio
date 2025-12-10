03500926f74419533b3af33dbd2345dc386b4be7
Ryotankobu <ucp4fya7@yahoo.co.jp>
2025-12-10 21:17:30 +1000

    POR-1008 Footer Section Base Design Completed

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,12 +1,43 @@
 <script setup>
+    import sampleImage from "@/assets/images/bellsprout.png";
 </script>
 
 <template>
-    <footer class="footer d-flex text-center flex-column py-3">
+    <footer class="footer d-flex text-center flex-column py-5">
         <div class="logo-area bg-danger mx-auto">
-            aaa
+            <div class="image-box mb-3">
+                
+                <h2>My Portfolio</h2>
+
+            </div>
+            
+            <div class="icon-box">
+                  <div class="row justify-content-center">
+                    <div class="col-1">
+                      <img
+                        :src="sampleImage"
+                        class="card-img-top w-25"
+                        alt="..."
+                      />
+                    </div>
+                    <div class="col-1">
+                      <img
+                        :src="sampleImage"
+                        class="card-img-top w-25"
+                        alt="..."
+                      />
+                    </div>
+                    <div class="col-1">
+                      <img
+                        :src="sampleImage"
+                        class="card-img-top w-25"
+                        alt="..."
+                      />
+                    </div>
+                  </div>
+                </div>
         </div>
-        <div class="footer-nav-area bg-success mx-auto">
+        <!-- <div class="footer-nav-area bg-success mx-auto mt-5">
             <ul class="nav text-center">
                 <li class="nav-item">
                     <a class="nav-link active" href="#">Active</a>
@@ -21,7 +52,7 @@
                     <a class="nav-link disabled" href="#">Disabled</a>
                 </li>
                 </ul>
-        </div>
+        </div> -->
   </footer>
 </template>
 
@@ -33,7 +64,6 @@
     margin-top: 0;
 }
 .logo-area {
-    height: 200px;
-    width: 400px;
+
 }
 </style>

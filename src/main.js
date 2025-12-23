96746ef94362399a379401766d9f7ee4723e9db9
Ryotankobu <ucp4fya7@yahoo.co.jp>
2025-12-23 17:50:00 +1000

    POR-1010 Project section completed

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 import './assets/styles/colors.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
 
 
 const app = createApp(App)
